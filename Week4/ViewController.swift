857abe9f1abba2f163a25475e8d15fbd40273b91
yeonupark <idepix@naver.com>
2023-08-07 12:27:21 +0900

    gitignore 설정

--- Week4/ViewController.swift
+++ Week4/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        callRequest()
     }
 
+    func callRequest() {
+        let url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20120101"
+        AF.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 
 }
 
